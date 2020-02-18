45189ecc411c3f5a1cbd5982cd81dc56ecb6a200
kanukam <49925341+kanukam@users.noreply.github.com>
2020-02-17 16:15:29 -0800

    Update youtubescrape.py

--- Youtube-Channel-Scraper/youtubescrape.py
+++ Youtube-Channel-Scraper/youtubescrape.py
@@ -25,38 +25,33 @@ def channel_data():
     l6.config(text=total_videos)
 
 
-def main():
-    # Create a tkinter window display interface
-    root = Tk()
-    root.title("Youtube Scraper")
-    # Sets display size to 500x200
-    root.geometry("500x200")
-    # User can enter into text box the channel URL
-    e1 = Entry(root, textvariable=StringVar())
-    # Setting size of text box
-    e1.grid(row=1, column=2)
-    # Creating a button which uses the channel_data function to get the URL, uses text boxes entry
-    b1 = Button(root, text="Enter Channel URL", command=channel_data)
-    b1.grid(row=2, column=2)
+# Create a tkinter window display interface
+root = Tk()
+root.title("Youtube Scraper")
+# Sets display size to 500x200
+root.geometry("500x200")
+# User can enter into text box the channel URL
+e1 = Entry(root, textvariable=StringVar())
+# Setting size of text box
+e1.grid(row=1, column=2)
+# Creating a button which uses the channel_data function to get the URL, uses text boxes entry
+b1 = Button(root, text="Enter Channel URL", command=channel_data)
+b1.grid(row=2, column=2)
 
-    # Setting labels on the tkinter display
-    l1 = Label(root, text="total number of subscribers : ", font="times 15 bold")
-    l1.grid(row=4, column=1)
-    l2 = Label(root, font="times 15 bold")
-    l2.grid(row=4, column=3)
+# Setting labels on the tkinter display
+l1 = Label(root, text="total number of subscribers : ", font="times 15 bold")
+l1.grid(row=4, column=1)
+l2 = Label(root, font="times 15 bold")
+l2.grid(row=4, column=3)
 
-    l3 = Label(root, text="total number of views : ", font="times 15 bold")
-    l3.grid(row=6, column=1)
-    l4 = Label(root, font="times 15 bold")
-    l4.grid(row=6, column=3)
+l3 = Label(root, text="total number of views : ", font="times 15 bold")
+l3.grid(row=6, column=1)
+l4 = Label(root, font="times 15 bold")
+l4.grid(row=6, column=3)
 
-    l5 = Label(root, text="total number of videos : ", font="times 15 bold")
-    l5.grid(row=8, column=1)
-    l6 = Label(root, font="times 15 bold")
-    l6.grid(row=8, column=3)
+l5 = Label(root, text="total number of videos : ", font="times 15 bold")
+l5.grid(row=8, column=1)
+l6 = Label(root, font="times 15 bold")
+l6.grid(row=8, column=3)
 
-    root.mainloop()
-
-
-if __name__ == '__main__':
-    main()
+root.mainloop()
